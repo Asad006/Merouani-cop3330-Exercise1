7256b3d89bd1a61b021ceba6501e8478892fb414
meroa006 <merouani.ass@gmail.com>
2021-06-03 09:33:12 -0400

    Exercise basic solution: completed

--- src/main/java/Exercise1/base/App.java
+++ src/main/java/Exercise1/base/App.java
@@ -1,4 +1,7 @@
 package Exercise1.base;
+
+import java.util.Scanner;
+
 /*
 Exercise 1 - Saying Hello
 Create a program that prompts for your name and prints a greeting using your name.
@@ -14,4 +17,25 @@ Write a version of the program that displays different greetings for different p
 Write a version of the program that displays the greeting in a GUI window.\\\\\
  */
 public class App {
+    public static void main(String[] args) {
+        App app = new App();
+
+        String input = app.getInput();
+
+        app.displayOutput(input);
+
+    }
+
+    private void displayOutput(String name) {
+
+        String output = "Hello, " + name + ", nice to meet you!";
+        System.out.println(output);
+    }
+
+    private String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        return name;
+    }
 }
---
+++ src/test/java/apptest.java
@@ -0,0 +1,2 @@
+public class apptest {
+}
